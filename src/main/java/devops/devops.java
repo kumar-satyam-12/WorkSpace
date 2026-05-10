30f6f41e04341a573c61621421879c7871b65861
Satyam <sk950400@gmail.com>
2026-05-10 12:00:19 +0530

    initiated

---
+++ src/main/java/devops/devops.java
@@ -0,0 +1,14 @@
+package devops;
+
+public class devops {
+
+	int sum (int x, int y) {
+		return x+y;
+	}
+	
+	
+	public static void main (String[] args) {
+		devops c1 = new devops();
+		System.out.println("Sum is "+c1.sum(10,20));
+	}
+}
